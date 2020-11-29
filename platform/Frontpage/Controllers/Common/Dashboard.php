bc9b555bc7e7d161f532a4b9a71e050001787cfb
Mang Phong <phong0am@gmail.com>
2020-11-29 09:36:56 +0700

    admin some source files

---
+++ platform/Frontpage/Controllers/Common/Dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Platform\Management\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Platform\Management\Controllers\Controler as Controler;
+
+class Hello extends Controller
+{
+    //code here
+}
